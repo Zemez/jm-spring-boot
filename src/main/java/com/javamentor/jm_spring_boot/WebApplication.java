9835b1403d0644201bf512911afecbe9c60e5d72
Andrey Inozemtsev <a.zemez@yandex.ru>
2019-08-28 01:11:36 +0300

    Init Spring Boot + Web + Thymeleaf + Security + Hibernate + MySql

---
+++ src/main/java/com/javamentor/jm_spring_boot/WebApplication.java
@@ -0,0 +1,13 @@
+package com.javamentor.jm_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+}
